05bff2b5b392a9c97f9b8f38af5a9346717bdf3c
promush <promush@kaist.ac.kr>
2017-06-03 17:46:32 +0900

    reservation date check

--- js/reservation.js
+++ js/reservation.js
@@ -1,4 +1,26 @@
 $(document).ready(function() {
+	var reserveBtn = document.getElementById('button-reserve');
+	var fromd = document.getElementById('from-date');
+	var tod = document.getElementById('to-date');
+
+    reserveBtn.onclick = function() {
+    	if((fromd.value == "") || (tod.value == "")){
+    		alert("체크인 / 체크아웃 날짜를 선택하셈")
+    	}
+    	else{
+    		var from_parts = fromd.value.split("/");
+    		var to_parts = tod.value.split("/");
+    		var f_date = new Date(from_parts[2],from_parts[0]-1,from_parts[1]);
+    		var t_date = new Date(to_parts[2],to_parts[0]-1,to_parts[1]);
+
+    		if(f_date > t_date){
+    			alert("체크아웃 날짜가 체크인 날짜보다는 뒤여야지 않겠니?");
+    		}
+    		else{
+		    	alert(fromd.value + " ~ " + tod.value);
+    		}
+    	}
+  }	
 	$('#from-date').datepicker({ 
 		language : 'ko', 
 		pickTime : false,
